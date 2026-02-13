929b4a063e7a326c512151f6e0d1a9a0cf09c631
GradientBooster <144033691+aditya0589@users.noreply.github.com>
2026-02-13 17:25:26 +0530

    Implement Kadane's algorithm for maximum subarray

---
+++ 53_maximum_subarray_kadanes_algorithm.py
@@ -0,0 +1,14 @@
+def kadane(arr):
+    max_sum = arr[0]
+    current_sum = arr[0]
+
+    for num in arr[1:]:
+        current_sum = max(num, current_sum + num)
+        max_sum = max(max_sum, current_sum)
+
+    return max_sum
+
+
+# Example
+arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+print(kadane(arr))   # Output: 6
